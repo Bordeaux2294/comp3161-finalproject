c7e857c68aa6138dc60c56a350983928d3fe69f2
princeloki <teriqstrachan@yahoo.com>
2023-03-28 12:08:30 -0500

    Created example for faker

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql');
-const apiRouter = require('./api/api_route')
+const apiRouter = require('./api/api_route');
+const { faker } = require('@faker-js/faker');
+
 require('dotenv/config')
 
 const app = express();
@@ -39,4 +41,12 @@ app.post('/login', (req, res) => {
 
 app.listen(process.env.PORT, ()=>{
     console.log('listening on port '+process.env.PORT);
+    for(let i=0;i<20;i++){
+        const first_name = faker.name.firstName();
+        const last_name = faker.name.lastName();
+        const email = faker.internet.email(first_name, last_name);
+        const address = faker.address.streetAddress() + ", " + faker.address.cityName() + ", Jamaica";
+        console.log(`First name: ${first_name} and last name: ${last_name} has an email
+        of ${email} and lives in ${address}`);
+    }
 })
